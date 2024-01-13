f3b523f137f3ca865beb6ee9a79a62f2bc4ecd62
Swiichy <adlpromail@gmail.com>
2024-01-13 05:56:32 +0100

    feat: Add SESSION_TOKEN_NAME environment variable to .env.example and update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import { env } from "@/env";
 export default withAuth({
   callbacks: {
     authorized: ({ req }) => {
+      // Refer to .env.example for the value of SESSION_TOKEN_NAME.
       const sessionToken = req.cookies.get(env.SESSION_TOKEN_NAME);
       if (!sessionToken) return false;
 
