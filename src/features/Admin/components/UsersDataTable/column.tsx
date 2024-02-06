3ab75ce6af297f913a36494a0c60275c620c742f
SwiichyCode <adlpromail@gmail.com>
2024-02-06 18:52:53 +0100

    fix: date rendering server error

--- src/features/Admin/components/UsersDataTable/column.tsx
+++ src/features/Admin/components/UsersDataTable/column.tsx
@@ -1,36 +1,37 @@
-"use client";
-import { cn } from "@/lib/utils";
-import type { ColumnDef } from "@tanstack/react-table";
-import type { User } from "@prisma/client";
+'use client';
+
+import { cn } from '@/lib/utils';
+import type { User } from '@prisma/client';
+import type { ColumnDef } from '@tanstack/react-table';
 
 const handleStyleRole = (role: string) => {
-  if (role === "dev") return "bg-blue-100 text-blue-800";
-  if (role === "admin") return "bg-green-100 text-green-800";
-  if (role === "user") return "bg-red-100 text-red-800";
-  return "bg-green-100 text-green-800";
+  if (role === 'dev') return 'bg-blue-100 text-blue-800';
+  if (role === 'admin') return 'bg-green-100 text-green-800';
+  if (role === 'user') return 'bg-red-100 text-red-800';
+  return 'bg-green-100 text-green-800';
 };
 
 export const userColumns: ColumnDef<User>[] = [
   {
-    accessorKey: "id",
-    header: "ID",
+    accessorKey: 'id',
+    header: 'ID',
   },
   {
-    accessorKey: "name",
-    header: "Name",
+    accessorKey: 'name',
+    header: 'Name',
   },
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: 'email',
+    header: 'Email',
   },
   {
-    accessorKey: "role",
-    header: "Role",
+    accessorKey: 'role',
+    header: 'Role',
     cell: ({ row }) => {
       const { role } = row.original;
 
       const roleClass = cn(
-        "inline-flex items-center px-2.5 py-1.5 rounded text-xs font-medium",
+        'inline-flex items-center px-2.5 py-1.5 rounded text-xs font-medium',
         handleStyleRole(role),
       );
 
@@ -38,14 +39,15 @@ export const userColumns: ColumnDef<User>[] = [
     },
   },
   {
-    accessorKey: "createdAt",
-    header: "Created on",
+    accessorKey: 'createdAt',
+    header: 'Created on',
     cell: ({ row }) => {
       const { createdAt } = row.original;
 
       return (
         <span>
-          {createdAt.toLocaleDateString()} à {createdAt.toLocaleTimeString()}
+          {createdAt.toLocaleDateString('en-EN')} à{' '}
+          {createdAt.toLocaleTimeString('en-EN')}
         </span>
       );
     },
